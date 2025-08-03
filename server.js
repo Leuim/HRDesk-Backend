4c967005cfb46c7711b261457c6dff51640035a1
leuim <jassim.mohammed48@gmail.com>
2025-08-03 13:33:12 +0300

    intial server.js config and create user model

---
+++ server.js
@@ -0,0 +1,24 @@
+const dotenv = require('dotenv')
+dotenv.config()
+const express = require('express')
+const morgan = require('morgan')
+const mongoose = require('mongoose')
+const app = express()
+const cors = require('cors')
+
+const PORT = process.env.PORT
+
+mongoose.connect(process.env.MONGODB_URI)
+
+mongoose.connection.on('connected',()=>{
+    console.log(`Connected to MongoDB ${mongoose.connection.name}`);
+})
+
+app.use(express.json())
+app.use(morgan('dev'))
+app.use(cors())
+
+
+app.listen(PORT,()=>{
+    console.log(`Listening on port: ${PORT}`);
+})
