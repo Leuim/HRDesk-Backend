0f2e83caa30160d15866a529ec9781d0f636c48c
leuim <jassim.mohammed48@gmail.com>
2025-08-07 15:44:33 +0300

    fix update and delete and index user route endpoints

--- controllers/users.js
+++ controllers/users.js
@@ -1,28 +1,36 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
+const LeaveBalance = require('../models/Leave Balance')
 const verifyToken = require('../middleware/verify-token');
 
-router.get('/',verifyToken,  async (req, res) => {
+router.get('/', verifyToken, async (req, res) => {
   try {
-    const users = await User.find({role:'employee'}, "name role");
-
-    res.json(users);
+    const users = await User.find().populate('leavebalance');
+    if (users.length === 0) {
+      res.status(404)
+      return res.status(200).json([]);
+    }
+    res.status(200).json(users);
   } catch (err) {
-    res.status(500).json({ err: err.message });
+    if (res.statusCode === 404) {
+      res.json({ err: err.message })
+    } else {
+      res.status(500).json({ err: err.message });
+    }
   }
 });
 
 router.get('/:userId', verifyToken, async (req, res) => {
   try {
-    if (req.user._id !== req.params.userId){
-      return res.status(403).json({ err: "Unauthorized"});
+    if (req.user._id !== req.params.userId) {
+      return res.status(403).json({ err: "Unauthorized" });
     }
 
     const user = await User.findById(req.params.userId);
 
     if (!user) {
-      return res.status(404).json({ err: 'User not found.'});
+      return res.status(404).json({ err: 'User not found.' });
     }
 
     res.json({ user });
@@ -31,25 +39,34 @@ router.get('/:userId', verifyToken, async (req, res) => {
   }
 });
 
-router.put('/:userId', verifyToken, async (req,res)=>{
+router.put('/:userId', verifyToken, async (req, res) => {
   try {
     const updatedUser = await User.findByIdAndUpdate(
       req.params.userId,
       req.body,
-      {new:true}
+      { new: true }
     )
     res.status(200).json(updatedUser)
   } catch (err) {
-    res.status(500).json({err:err.message})
+    res.status(500).json({ err: err.message })
   }
 })
 
-router.delete('/:userId', verifyToken, async (req,res)=>{
+router.delete('/:userId', verifyToken, async (req, res) => {
   try {
     const deletedUser = await User.findByIdAndDelete(req.params.userId)
+
+    if (!deletedUser) {
+      return res.status(404).json({ err: 'User not found' })
+    }
+
+    if (deletedUser.leavebalance) {
+      await LeaveBalance.findByIdAndDelete(deletedUser.leavebalance)
+    }
+
     res.status(200).json(deletedUser)
   } catch (error) {
-    res.status(500).json({err:err.message})
+    res.status(500).json({ err: error.message })
   }
 })
 
